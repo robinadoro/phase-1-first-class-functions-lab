303238292cbc79d4c4564b9717cd741b3a3bdc7f
Robinson Idagiza <robinadoro@gmail.com>
2022-04-25 00:24:46 +0300

    commit tree af54a6d0757d1cb4bae6f7aca41ab994543bb996

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(){
+    return drivers.slice(0, 2)
+}
+
+function returnLastTwoDrivers(){
+    return drivers.slice(-2)
+}
+
+let selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers];
+selectingDrivers[0];
+
+function createFareMultiplier(fareMultiplier){
+    return function(fareQuintupler){
+        return fareMultiplier*fareMultiplier;
+    
+    }
+}
+
+function fareDoubler(fare){
+    return fare*2
+}
+ function fareTripler(fares){
+     return fares*3
+ }
+
+function selectDifferentDrivers(drivers, arrDrivers){
+    return arrDrivers(drivers)
+
+}
+
